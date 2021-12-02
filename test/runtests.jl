306d5c50a40c32fe2f11fe907616e7ec8285d0b1
Nicholas Bauer <nicholasbauer@outlook.com>
2021-12-02 14:13:22 -0500

    default tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -5,12 +5,46 @@ for i ∈ (1, 2)
     if i == 1
         testname = "checked default"
         @default_checked
+        
+        @testset "$testname" begin
+            @test_throws OverflowError typemax(Int) + 1
+            @test_throws OverflowError typemin(Int) - 1
+            @test_throws OverflowError typemax(UInt) + 1
+            @test_throws OverflowError typemin(UInt) - 1
+
+            @test_throws OverflowError typemax(Int) * 2
+            @test_throws OverflowError typemin(Int) * 2
+            @test_throws OverflowError typemax(UInt) * 2
+
+            # @test_throws OverflowError typemax(Int) ^ 2
+            # @test_throws OverflowError typemin(Int) ^ 2
+            # @test_throws OverflowError typemax(UInt) ^ 2
+
+            @test_throws OverflowError abs(typemin(Int))
+        end
     else
         testname = "unchecked default"
         @default_unchecked
+
+        @testset "$testname" begin
+            @test typemax(Int) + 1 == typemin(Int)
+            @test typemin(Int) - 1 == typemax(Int)
+            @test typemax(UInt) + 1 == typemin(UInt)
+            @test typemin(UInt) - 1 == typemax(UInt)
+
+            @test typemax(Int) * 2 == -2
+            @test typemin(Int) * 2 == 0
+            @test typemax(UInt) * 2 == 0xfffffffffffffffe
+
+            # @test typemax(Int) ^ 2 == 1
+            # @test typemin(Int) ^ 2 == 0
+            # @test typemax(UInt) ^ 2 == 0x0000000000000001
+
+            @test abs(typemin(Int)) == typemin(Int)
+        end
     end
 
-    @testset "$testname" begin
+    @testset "$testname with expressions" begin
         @testset "checked expressions" begin
             @test_throws OverflowError @checked typemax(Int) + 1
             @test_throws OverflowError @checked typemin(Int) - 1
