6e937a5e567a8927045f74c7e103d770c6b6fc07
Nicholas Bauer <nicholasbauer@outlook.com>
2021-12-04 00:17:19 -0500

    Add Revise compat

--- src/OverflowContexts.jl
+++ src/OverflowContexts.jl
@@ -10,6 +10,7 @@ include("unchecked_compat.jl")
 function __init__()
     @require SHA = "ea8e919c-243c-51af-8825-aaa63cd721ce" include("SHA_compat.jl")
     @require Random = "9a3f8284-a2c9-5f02-9a11-845980a1fd5c" include("Random_compat.jl")
+    @require Revise = "295af30f-e4ad-537b-8983-00126c2a3abe" include("Revise_compat.jl")
 end
 
 export @default_checked, @default_unchecked, @checked, @unchecked,
