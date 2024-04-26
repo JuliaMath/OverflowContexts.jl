cbeadb65954b92838c2bffe37e4ceb7599229ddd
Nicholas Bauer <nicholasbauer@outlook.com>
2024-04-26 11:57:18 -0400

    Adjust tests for default_ methods and error handling

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,135 +1,79 @@
 using Test
 using OverflowContexts
 
-for i ∈ (1, 2)
-    if i == 1
-        testname = "checked default"
-        @default_checked
-        
-        @testset "$testname" begin
-            @test_throws OverflowError -typemin(Int)
-            @test_throws OverflowError -UInt(1)
+@testset "checked expressions" begin
+    @test_throws OverflowError @checked -typemin(Int)
+    @test_throws OverflowError @checked -UInt(1)
 
-            @test_throws OverflowError typemax(Int) + 1
-            @test_throws OverflowError typemax(UInt) + 1
+    @test_throws OverflowError @checked typemax(Int) + 1
+    @test_throws OverflowError @checked typemax(UInt) + 1
+    
+    @test_throws OverflowError @checked typemin(Int) - 1
+    @test_throws OverflowError @checked typemin(UInt) - 1
 
-            @test_throws OverflowError typemin(Int) - 1
-            @test_throws OverflowError typemin(UInt) - 1
+    @test_throws OverflowError @checked typemax(Int) * 2
+    @test_throws OverflowError @checked typemin(Int) * 2
+    @test_throws OverflowError @checked typemax(UInt) * 2
 
-            @test_throws OverflowError typemax(Int) * 2
-            @test_throws OverflowError typemin(Int) * 2
-            @test_throws OverflowError typemax(UInt) * 2
+    @test_throws OverflowError @checked typemax(Int) ^ 2
+    @test_throws OverflowError @checked typemin(Int) ^ 2
+    @test_throws OverflowError @checked typemax(UInt) ^ 2
 
-            @test_throws OverflowError typemax(Int) ^ 2
-            @test_throws OverflowError typemin(Int) ^ 2
-            @test_throws OverflowError typemax(UInt) ^ 2
+    @test_throws OverflowError @checked abs(typemin(Int))
+end
 
-            @test_throws OverflowError abs(typemin(Int))
-        end
-    else
-        testname = "unchecked default"
-        @default_unchecked
+@testset "unchecked expressions" begin
+    @test @unchecked -typemin(Int) == typemin(Int)
+    @test @unchecked -UInt(1) == typemax(UInt)
 
-        @testset "$testname" begin
-            @test -typemin(Int) == typemin(Int)
-            @test -UInt(1) == typemax(UInt)
+    @test @unchecked typemax(Int) + 1 == typemin(Int)
+    @test @unchecked typemax(UInt) + 1 == typemin(UInt)
 
-            @test typemax(Int) + 1 == typemin(Int)
-            @test typemax(UInt) + 1 == typemin(UInt)
+    @test @unchecked typemin(Int) - 1 == typemax(Int)
+    @test @unchecked typemin(UInt) - 1 == typemax(UInt)
 
-            @test typemin(Int) - 1 == typemax(Int)
-            @test typemin(UInt) - 1 == typemax(UInt)
+    @test @unchecked typemax(Int) * 2 == -2
+    @test @unchecked typemin(Int) * 2 == 0
+    @test @unchecked typemax(UInt) * 2 == typemax(UInt) - 1
 
-            @test typemax(Int) * 2 == -2
-            @test typemin(Int) * 2 == 0
-            @test typemax(UInt) * 2 == typemax(UInt) - 1
+    @test @unchecked typemax(Int) ^ 2 == 1
+    @test @unchecked typemin(Int) ^ 2 == 0
+    @test @unchecked typemax(UInt) ^ 2 == UInt(1)
 
-            @test typemax(Int) ^ 2 == 1
-            @test typemin(Int) ^ 2 == 0
-            @test typemax(UInt) ^ 2 == UInt(1)
+    @test @unchecked abs(typemin(Int)) == typemin(Int)
+end
 
-            @test abs(typemin(Int)) == typemin(Int)
-        end
+@testset "lowest-level macro takes priority" begin
+    @checked begin
+        @test @unchecked typemax(Int) + 1 == typemin(Int)
     end
-
-    @testset "$testname with expressions" begin
-        @testset "checked expressions" begin
-            @test_throws OverflowError @checked -typemin(Int)
-            @test_throws OverflowError @checked -UInt(1)
-
-            @test_throws OverflowError @checked typemax(Int) + 1
-            @test_throws OverflowError @checked typemax(UInt) + 1
-            
-            @test_throws OverflowError @checked typemin(Int) - 1
-            @test_throws OverflowError @checked typemin(UInt) - 1
-    
-            @test_throws OverflowError @checked typemax(Int) * 2
-            @test_throws OverflowError @checked typemin(Int) * 2
-            @test_throws OverflowError @checked typemax(UInt) * 2
-    
-            @test_throws OverflowError @checked typemax(Int) ^ 2
-            @test_throws OverflowError @checked typemin(Int) ^ 2
-            @test_throws OverflowError @checked typemax(UInt) ^ 2
-    
-            @test_throws OverflowError @checked abs(typemin(Int))
-        end
-    
-        @testset "unchecked expressions" begin
-            @test @unchecked -typemin(Int) == typemin(Int)
-            @test @unchecked -UInt(1) == typemax(UInt)
-
-            @test @unchecked typemax(Int) + 1 == typemin(Int)
-            @test @unchecked typemax(UInt) + 1 == typemin(UInt)
-
-            @test @unchecked typemin(Int) - 1 == typemax(Int)
-            @test @unchecked typemin(UInt) - 1 == typemax(UInt)
-    
-            @test @unchecked typemax(Int) * 2 == -2
-            @test @unchecked typemin(Int) * 2 == 0
-            @test @unchecked typemax(UInt) * 2 == typemax(UInt) - 1
-    
-            @test @unchecked typemax(Int) ^ 2 == 1
-            @test @unchecked typemin(Int) ^ 2 == 0
-            @test @unchecked typemax(UInt) ^ 2 == UInt(1)
-    
-            @test @unchecked abs(typemin(Int)) == typemin(Int)
-        end
-    
-        @testset "lowest-level macro takes priority" begin
-            @checked begin
-                @test @unchecked typemax(Int) + 1 == typemin(Int)
-            end
-            @unchecked begin
-                @test_throws OverflowError @checked typemax(Int) + 1
-            end
-        end
-
-        @testset "non-integer math still works" begin
-            @test @checked -1.0 == -1
-            @test @unchecked -1.0 == -1
-            @test @checked 1.0 + 3.0 == 4.0
-            @test @unchecked 1.0 + 3.0 == 4.0
-            @test @checked 1 + 3.0 == 4.0
-            @test @unchecked 1 + 3.0 == 4.0
-            @test @checked 1.0 - 3.0 == -2.0
-            @test @unchecked 1.0 - 3.0 == -2.0
-            @test @checked 1 - 3.0 == -2.0
-            @test @unchecked 1 - 3.0 == -2.0
-            @test @checked 1.0 * 3.0 == 3.0
-            @test @unchecked 1.0 * 3.0 == 3.0
-            @test @checked 1 * 3.0 == 3.0
-            @test @unchecked 1 * 3.0 == 3.0
-            @test @checked 1.0 ^ 3.0 == 1.0
-            @test @unchecked 1.0 ^ 3.0 == 1.0
-            @test @checked 1 ^ 3.0 == 1.0
-            @test @unchecked 1 ^ 3.0 == 1.0
-            @test @checked abs(-1.0) == 1.0
-            @test @unchecked abs(-1.0) == 1.0
-        end
+    @unchecked begin
+        @test_throws OverflowError @checked typemax(Int) + 1
     end
 end
 
-@default_unchecked # reset to Julia default
+@testset "non-integer math still works" begin
+    @test @checked -1.0 == -1
+    @test @unchecked -1.0 == -1
+    @test @checked 1.0 + 3.0 == 4.0
+    @test @unchecked 1.0 + 3.0 == 4.0
+    @test @checked 1 + 3.0 == 4.0
+    @test @unchecked 1 + 3.0 == 4.0
+    @test @checked 1.0 - 3.0 == -2.0
+    @test @unchecked 1.0 - 3.0 == -2.0
+    @test @checked 1 - 3.0 == -2.0
+    @test @unchecked 1 - 3.0 == -2.0
+    @test @checked 1.0 * 3.0 == 3.0
+    @test @unchecked 1.0 * 3.0 == 3.0
+    @test @checked 1 * 3.0 == 3.0
+    @test @unchecked 1 * 3.0 == 3.0
+    @test @checked 1.0 ^ 3.0 == 1.0
+    @test @unchecked 1.0 ^ 3.0 == 1.0
+    @test @checked 1 ^ 3.0 == 1.0
+    @test @unchecked 1 ^ 3.0 == 1.0
+    @test @checked abs(-1.0) == 1.0
+    @test @unchecked abs(-1.0) == 1.0
+end
 
 @testset "symbol replacement" begin
     expr = @macroexpand @checked foldl(+, [])
@@ -218,6 +162,19 @@ end
     CheckedModule.NestedUncheckedModule.testfunc()
     UncheckedModule.testfunc()
     UncheckedModule.NestedCheckedModule.testfunc()
+    
+    # Trying to set a default after referencing the operator should error
+    module BadCheckedModule
+        using OverflowContexts, Test
+        x = 1 + 1
+        @test_throws ErrorException @default_checked
+        @test -(typemin(Int)) == typemin(Int) # checked methods reversed to Base on error
+    end
+    module BadUncheckedModule
+        using OverflowContexts, Test
+        x = 1 + 1
+        @test_throws ErrorException @default_unchecked
+    end
 end
 
 @testset "ensure pow methods don't promote on the power" begin
