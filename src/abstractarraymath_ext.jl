3028a82f483e4d3879d4934ca8973bb86da083d5
Nicholas Bauer <nicholasbauer@outlook.com>
2024-05-17 14:36:36 -0400

    Fix compat usage

--- src/abstractarraymath_ext.jl
+++ src/abstractarraymath_ext.jl
@@ -2,16 +2,19 @@ import Base.Checked: checked_neg, checked_add, checked_sub, checked_mul
 if VERSION ≥ v"1.11-alpha"
     import Base.Checked: checked_pow
 end
+if VERSION ≥ v"1.2"
+    using Base: broadcast_preserving_zero_d
+end
 
-checked_neg(A::AbstractArray) = Base.broadcast_preserving_zero_d(checked_neg, A)
+checked_neg(A::AbstractArray) = broadcast_preserving_zero_d(checked_neg, A)
 for f in (:checked_add, :checked_sub)
     @eval function ($f)(A::AbstractArray, B::AbstractArray)
         promote_shape(A, B) # check size compatibility
-        Base.broadcast_preserving_zero_d($f, A, B)
+        broadcast_preserving_zero_d($f, A, B)
     end
 end
-checked_mul(A::Number, B::AbstractArray) = Base.broadcast_preserving_zero_d(checked_mul, B, A)
-checked_mul(A::AbstractArray, B::Number) = Base.broadcast_preserving_zero_d(checked_mul, A, B)
+checked_mul(A::Number, B::AbstractArray) = broadcast_preserving_zero_d(checked_mul, B, A)
+checked_mul(A::AbstractArray, B::Number) = broadcast_preserving_zero_d(checked_mul, A, B)
 checked_mul(A::AbstractArray, B::AbstractArray) = error("Checked matrix multiplication is not available")
 
 checked_pow(A::AbstractArray, B::Number) = error("Checked matrix multiplication is not available")
