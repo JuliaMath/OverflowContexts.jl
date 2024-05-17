4a9660759fdd52876f577325c70e8d10bd7753a1
Nicholas Bauer <nicholasbauer@outlook.com>
2024-05-17 14:47:49 -0400

    Fix compat again

--- src/abstractarraymath_ext.jl
+++ src/abstractarraymath_ext.jl
@@ -19,11 +19,16 @@ checked_mul(A::AbstractArray, B::AbstractArray) = error("Checked matrix multipli
 
 checked_pow(A::AbstractArray, B::Number) = error("Checked matrix multiplication is not available")
 
+# Compatibility with Julia 1.0 and 1.1
 if VERSION < v"1.2"
-    # Compatibility with Julia 1.0 and 1.1
+    if VERSION < v"1.1"
+        @inline materialize(bc::Base.Broadcast.Broadcasted) = copy(instantiate(bc))
+    else
+        using Base.Broadcast: materialize
+    end
     @inline function broadcast_preserving_zero_d(f, As...)
-        bc = Base.broadcasted(f, As...)
-        r = Base.materialize(bc)
+        bc = Base.Broadcast.broadcasted(f, As...)
+        r = materialize(bc)
         return length(axes(bc)) == 0 ? fill!(similar(bc, typeof(r)), r) : r
     end
 end
