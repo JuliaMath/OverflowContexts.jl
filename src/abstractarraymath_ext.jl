c15b8d30e641ab448d2db7821865bb7097143186
Nicholas Bauer <nicholasbauer@outlook.com>
2024-05-17 12:55:31 -0400

    Error for matrix multiplication

--- src/abstractarraymath_ext.jl
+++ src/abstractarraymath_ext.jl
@@ -1,10 +1,9 @@
 checked_neg(A::AbstractArray) = broadcast_preserving_zero_d(checked_neg, A)
-
 for f in (:checked_add, :checked_sub)
     @eval function ($f)(A::AbstractArray, B::AbstractArray)
         promote_shape(A, B) # check size compatibility
         Base.broadcast_preserving_zero_d($f, A, B)
     end
 end
-
 checked_mul(A::AbstractArray, B::Number) = Base.broadcast_preserving_zero_d(checked_mul, A, B)
+checked_mul(A::AbstractArray, B::AbstractArray) = throw(NotImpementedError("Checked matrix multiplication is not available"))
