29eaab729cef26c197f37166455ac63662d2ec6d
Nicholas Bauer <nicholasbauer@outlook.com>
2024-05-17 14:05:34 -0400

    Add tests, pow error, more

--- src/OverflowContexts.jl
+++ src/OverflowContexts.jl
@@ -2,6 +2,7 @@ module OverflowContexts
 
 include("macros.jl")
 include("base_ext.jl")
+include("abstractarraymath_ext.jl")
 
 export @default_checked, @default_unchecked, @checked, @unchecked,
     unchecked_neg, unchecked_add, unchecked_sub, unchecked_mul, unchecked_negsub, unchecked_pow, unchecked_abs,
