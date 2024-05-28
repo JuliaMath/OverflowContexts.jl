40fdf57bf6c7b8eca909a8ab666ef4ef491163d7
Nicholas Bauer <nicholasbauer@outlook.com>
2024-05-28 11:38:50 -0400

    Remove exports for div methods

--- src/OverflowContexts.jl
+++ src/OverflowContexts.jl
@@ -5,9 +5,6 @@ include("base_ext.jl")
 include("base_ext_sat.jl")
 include("abstractarraymath_ext.jl")
 
-export @default_checked, @default_unchecked, @default_saturating, @checked, @unchecked, @saturating,
-    checked_div, checked_fld, checked_cld, checked_rem, checked_mod,
-    unchecked_div, unchecked_fld, unchecked_cld, unchecked_rem, unchecked_mod,
-    saturating_div, saturating_fld, saturating_cld, saturating_rem, saturating_mod
+export @default_checked, @default_unchecked, @default_saturating, @checked, @unchecked, @saturating
 
 end # module
